7df7ec0a6cab8e61965009148bbfeeb82b7fcf71
Ivan Rodriguez <ivancalvo91@gmail.com>
2022-07-18 12:57:50 -0500

    creating more practice

--- gettingStarted/helloworld/Program.fs
+++ gettingStarted/helloworld/Program.fs
@@ -1,11 +1,16 @@
 ﻿open System
 
 [<EntryPoint>]
-let main arg =
+let main (arg: string[]) =
     let person =
         if arg.Length > 0 then
             arg.[0]
         else
             "Anonimous world here"
-    printf "Hello %s from the F# world!" person
+    for i in 0..arg.Length-1 do
+        let person2 = arg.[i]
+        printfn " Hello %s from f#" person2
+    printfn "Nice to meet you, %s?" person
     0
+
+
